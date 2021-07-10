e92761de498b013c96c4492b08213581510bdf6f
Arto Chydenius <arto.chydenius@reaktor.com>
2021-07-10 19:47:24 +0300

    Initial Webpack setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unburn It
+
+Install dependencies with `npm install`. Start in development mode with `npm start` (development server responds at http://localhost:9000). Create a production build with `npm run build`.
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  }
+};
