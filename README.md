aaa1888be87cbbfe00bddb210260e75d8adf2c4e
Arto Chydenius <arto.chydenius@reaktor.com>
2021-07-25 12:00:57 +0300

    Add configuration for infrastructure

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 - start app in development mode with `npm start` (the development server responds at http://localhost:9000)
 - lint code with `npm run lint`
 - create a production build with `npm run build`
+
+## Hosting & deployment
+
+The project is hosted as a static site in Amazon S3.
+
+### Prerequisites:
+
+- [AWS CLI](https://aws.amazon.com/cli/) with default credentials and region configured
+- [AWS CDK](https://aws.amazon.com/cdk/)
+- Registered domain in [Amazon Route 53](https://aws.amazon.com/route53/)
+
+### Deployment:
+
+- Initialize CDK with `cdk bootstrap` (needs to be done only once)
+- Deploy with `cdk deploy`
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,108 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as route53 from '@aws-cdk/aws-route53'
+import * as s3 from '@aws-cdk/aws-s3'
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
+import * as acm from '@aws-cdk/aws-certificatemanager'
+import * as targets from '@aws-cdk/aws-route53-targets'
+import * as cloudfront from '@aws-cdk/aws-cloudfront'
+import * as iam from '@aws-cdk/aws-iam'
 
-export class InfrastructureStack extends cdk.Stack {
+const domainName = 'unburn.it'
+
+export default class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      domainName,
+    })
+    const domain = `www.${domainName}`
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+      this,
+      'OriginAccessIdentity'
+    )
+    new cdk.CfnOutput(this, 'SiteUrl', { value: `http://${domain}` })
+
+    const bucket = new s3.Bucket(this, 'Bucket', {
+      bucketName: domain,
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    })
+
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [bucket.arnForObjects('*')],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    )
+    new cdk.CfnOutput(this, 'BucketName', { value: bucket.bucketName })
+
+    const certificate = new acm.DnsValidatedCertificate(this, 'Certificate', {
+      domainName: domain,
+      hostedZone,
+      region: 'us-east-1',
+    })
+    new cdk.CfnOutput(this, 'CertificateArn', {
+      value: certificate.certificateArn,
+    })
+
+    const viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
+      certificate,
+      {
+        sslMethod: cloudfront.SSLMethod.SNI,
+        securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
+        aliases: [domain],
+      }
+    )
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      'Distribution',
+      {
+        viewerCertificate,
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+              originAccessIdentity,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+                compress: true,
+                allowedMethods:
+                  cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+              },
+            ],
+          },
+        ],
+      }
+    )
+    new cdk.CfnOutput(this, 'DistributionId', {
+      value: distribution.distributionId,
+    })
+
+    new route53.ARecord(this, 'SiteAliasRecord', {
+      recordName: domain,
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(distribution)
+      ),
+      zone: hostedZone,
+    })
+
+    new s3Deployment.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [s3Deployment.Source.asset('../site')],
+      destinationBucket: bucket,
+      distribution,
+      distributionPaths: ['/*'],
+    })
   }
 }
