ba7c4a0f66cd931f465c2d81d5f27455d7b46219
Arto Chydenius <arto.chydenius@reaktor.com>
2021-08-07 14:08:59 +0300

    Add social sharing metadata

--- placeholder/webpack.config.js
+++ placeholder/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const WebpackPwaManifest = require('webpack-pwa-manifest')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = (env, argv) => ({
   entry: './src/index.js',
@@ -29,6 +30,20 @@ module.exports = (env, argv) => ({
     new HtmlWebpackPlugin({
       template: './src/index.html',
       favicon: './src/assets/favicon.ico',
+      meta: {
+        'og:title': { property: 'og:title', content: 'Unburn It' },
+        'og:description': {
+          property: 'og:description',
+          content:
+            'Unburn It is a song that you can play like a game. Upon pressing play you sink through the screen space and your mouse movement changes the structure of the music that flows around. The seed of the composition itself is an eco-greif dirge, which can be bent and morphed into a burner, a banger, and/or disjointed fragments. The amount of interaction is up to you. Altogether the work functions as a composition, an instrument, and a game.',
+        },
+        'og:image': {
+          property: 'og:image',
+          content: 'https://unburn.it/social_sharing_image1-compressed.jpg',
+        },
+        'og:url': { property: 'og:url', content: 'https://unburn.it' },
+        'twitter:card': 'summary_large_image',
+      },
     }),
     new MiniCssExtractPlugin(),
     new WebpackPwaManifest({
@@ -38,6 +53,9 @@ module.exports = (env, argv) => ({
         { src: path.resolve('./src/assets/icon-192.png'), sizes: '192x192' },
         { src: path.resolve('./src/assets/icon-512.png'), sizes: '512x512' },
       ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: './src/assets/social_sharing_image1-compressed.jpg' }],
     }),
   ],
   devServer: {
