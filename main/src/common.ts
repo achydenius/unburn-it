fddcfd8ed1dd99bf64f02f14fbea682044dbe371
Arto Chydenius <arto.chydenius@reaktor.com>
2021-09-06 10:43:56 +0300

    Change ambient sound order

--- main/src/common.ts
+++ main/src/common.ts
@@ -1,7 +1,7 @@
 import ambient1 from '../assets/common/V1_AMBIENT-128.mp3'
-import ambient2 from '../assets/common/V2_AMBIENT-128.mp3'
-import ambient3 from '../assets/common/V3_AMBIENT-128.mp3'
-import ambient4 from '../assets/common/V4_AMBIENT-128.mp3'
+import ambient2 from '../assets/common/V4_AMBIENT-128.mp3'
+import ambient3 from '../assets/common/V2_AMBIENT-128.mp3'
+import ambient4 from '../assets/common/V3_AMBIENT-128.mp3'
 
 const ambientSounds = {
   ambient1,
